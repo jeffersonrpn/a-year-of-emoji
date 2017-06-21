2193137266373f15162e6d9d7bb790096fcaab0a
Jefferson Neves <jefferson.rpn@gmail.com>
2017-06-21 11:59:37 -0300

    Do a barrel roll

--- scraping.py
+++ scraping.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import re
 from bs4 import BeautifulSoup
 import requests
 
@@ -9,8 +9,14 @@ soup = BeautifulSoup(r.text, 'html.parser')
 messages = soup.find_all('div', {'class': 'im_message_body'})
 
 csv = open('messages.csv', 'wb')
+csv.write('author,emojis')
 for message in messages:
-    autor = message.find('a', {'class': 'im_message_author'}).get_text()
+    author = message.find('a', {'class': 'im_message_author'}).get_text()
     text = message.find('div', {'class': 'im_message_text'}).get_text()
-    csv.write(autor.encode('utf-8')+', '+text.encode('utf-8')+'\n')
+    try:
+        match = re.findall(':[A-z]+:', text)
+        emojis = ','.join(match)
+    except:
+        emojis = ''
+    csv.write(author.encode('utf-8')+',"'+emojis+'"\n')
 csv.close()

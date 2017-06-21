2cb955c98d23c27c083e7b2fef1c105ac2cbe29a
Jefferson Neves <jefferson.rpn@gmail.com>
2017-06-21 17:48:59 -0300

    Clear the html

---
+++ cleaning.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('http://localhost:3000/')
+soup = BeautifulSoup(r.text, 'html.parser')
+messages = soup.find_all('div', {'class': 'im_history_message_wrap'})
+
+html = open('messages.html', 'wb')
+for message in messages:
+    html.write(message)
+html.close()

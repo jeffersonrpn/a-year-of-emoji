498ef5a3e1678c65d6bf815d567a9f6aeb4192fe
Jefferson Neves <jefferson.rpn@gmail.com>
2017-06-23 11:57:29 -0300

    Now with message date

--- scraping.py
+++ scraping.py
@@ -6,18 +6,30 @@ import requests
 
 r = requests.get('http://localhost:3000/messages.html')
 soup = BeautifulSoup(r.text, 'html.parser')
-messages = soup.find_all('div', {'class': 'im_message_body'})
+messages = soup.find_all('div', {'class': 'im_history_message_wrap'})
 
 csv = open('messages.csv', 'wb')
-csv.write('author,emojis\n')
+csv.write('author,emojis,date\n')
+messageDate = ''
 for message in messages:
-    author = message.find('a', {'class': 'im_message_author'}).get_text()
-    text = message.find('div', {'class': 'im_message_text'}).get_text()
+    author = message.find('a', {'class': 'im_message_author'})
+    if author:
+        messageAuthor = author.get_text()
+    else:
+        messageAuthor = "ERROR"
+    text = message.find('div', {'class': 'im_message_text'})
+    if text:
+        messageText = text.get_text()
+    else:
+        messageText = "ERROR MSG"
+    date = message.find('span', {'class': 'im_message_date_split_text'})
+    if date:
+        messageDate = date.get_text()
     try:
-        match = re.findall(':[A-z]+:', text)
+        match = re.findall(':[A-z]+:', messageText)
         emojis = ','.join(match)
     except:
         emojis = ''
     if emojis:
-        csv.write(author.encode('utf-8')+',"'+emojis+'"\n')
+        csv.write(messageAuthor.encode('utf-8')+',"'+emojis+'","'+messageDate.encode('utf-8')+'"\n')
 csv.close()

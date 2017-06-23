a70818cda9768711b3bcd87e00030c1a4815c863
Jefferson Neves <jefferson.rpn@gmail.com>
2017-06-23 12:17:05 -0300

    Break the chain with months

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var container = d3.select("#emojis");
+var parseTime = d3.timeParse("%A, %B %e, %Y");
+var formatTime = d3.timeFormat("%b");
 
+var currentMonth = "";
 d3.csv("messages.csv", function(data) {
   data.forEach(function(message) {
     var author = (message.author).substring(0, 1);
+    var date = parseTime(message.date);
+    var month = formatTime(date);
+    if (month != currentMonth) {
+      container.append("div")
+        .attr("class", "month")
+        .text(month);
+      currentMonth = month;
+    }
     emojis = message.emojis.split(',')
     for (var i = 0; i < emojis.length; i++) {
       var emoji = emojione.shortnameToImage(emojis[i]);
       container.append("div")
         .attr("class", "emoji "+author)
-        // .style("background-color", "#8e44ad")
         .html(emoji);
     }
   });

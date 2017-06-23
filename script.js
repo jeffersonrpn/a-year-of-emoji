d3f9b04b6853b2d5392a17cb0ca26e0c3ad88816
Jefferson Neves <jefferson.rpn@gmail.com>
2017-06-23 16:48:34 -0300

    Some style :sparkles:

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var container = d3.select("#emojis");
+var monthContainer;
+var monthTitle;
 var parseTime = d3.timeParse("%A, %B %e, %Y");
-var formatTime = d3.timeFormat("%b");
+var formatTime = d3.timeFormat("%B");
 
 var currentMonth = "";
 d3.csv("messages.csv", function(data) {
@@ -9,15 +11,16 @@ d3.csv("messages.csv", function(data) {
     var date = parseTime(message.date);
     var month = formatTime(date);
     if (month != currentMonth) {
-      container.append("div")
-        .attr("class", "month")
-        .text(month);
+      monthContainer  = container.append("div").attr("class", "month");
+      monthTitle      = monthContainer.append("div").attr("class", "month-title");
+      monthTitle.append("div").attr("class", "month-year").text(d3.timeFormat("%Y")(date));
+      monthTitle.append("div").attr("class", "month-name").text(month);
       currentMonth = month;
     }
     emojis = message.emojis.split(',')
     for (var i = 0; i < emojis.length; i++) {
       var emoji = emojione.shortnameToImage(emojis[i]);
-      container.append("div")
+      monthContainer.append("div")
         .attr("class", "emoji "+author)
         .html(emoji);
     }
